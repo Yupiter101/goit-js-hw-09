b22bcbb5e5cd28f4622ecbcb64845946ac14b53a
Sergey_Sanyuk <shv08@ukr.net>
2023-04-14 18:41:09 +0300

    V6

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+console.log('test3');
+
+// ======= Submit Btn =======
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    const elements = event.currentTarget.elements;
+    const delay = Number(elements.delay.value);
+    const step = Number(elements.step.value);
+    const amount = Number(elements.amount.value);
+    event.currentTarget.reset();
+    makeSeveralPromise(delay, step, amount);
 }
+// ======= Submit Btn /Конец =======
+
+
+function makeSeveralPromise(delay, step, amount) {
+    let delayStart = delay;
+    for(let i=1; i<=amount; i++) {
+        console.log(i, delayStart);
+        
+        createPromise(i, delayStart)
+        .then(({position, delay}) => {console.log(`✅ Fulfilled promise ${position} in ${delay}ms`); })
+        .catch(({position, delay}) => {console.log(`❌ Rejected promise ${position} in ${delay}ms`); })
+        .finally(() => { });
+        
+        delayStart += step;
+    }
+}
+
+
+// ==== Promise =====
+
+function createPromise(position, delay) { //1.2.3...  1000
+    const shouldResolve = Math.random() > 0.5;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({position, delay}); //, position, delay  / 'ку-ку это resolve'
+              } else {
+                reject({position, delay}); //, position, delay / 'ку-ку это reject'
+              }
+        }, delay);
+    });  
+  };
+
+// ==== Promise /Конец =====
+
+console.log('By Конец');
